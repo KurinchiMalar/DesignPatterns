4e15214ec0d10c51ee1f8f7f9ffa80fb00ad1240
KurinchiMalar <kurinchimalar.n@gmail.com>
2024-05-08 08:51:01 +0530

    SingleTonCommit

--- corePatternsUdemy/src/corePatterns/singleton/DateUtil.java
+++ corePatternsUdemy/src/corePatterns/singleton/DateUtil.java
@@ -1,5 +1,18 @@
 package corePatterns.singleton;
 
 public class DateUtil {
+	
+	private static DateUtil instance;
+	
+	private DateUtil() {
+		
+	}
+	
+	public DateUtil getInstance() {
+		if(instance == null) {
+			instance = new DateUtil();
+		}
+		return instance;
+	}
 
 }
